e2b31b90b16d00694c15d34a5f550ae8079d0985
James-Chann <jameschan51@163.com>
2018-07-18 16:46:41 +0800

    finish init and header-style finish

---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="header">
+    <!--头部图片-->
+    <div class="header-icon"></div>
+    <!--头部标题-->
+    <div class="header-txt">Chicken Music</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~common/stylus/mixin.styl';
+@import '~common/stylus/variable.styl'
+
+  .header
+    width 100%
+    height 44px
+    line-height 44px
+    text-align center
+    font-size 0
+  /*===头部图片==================================================================================================================================================*/
+    .header-icon
+        display inline-block
+        width 30px
+        height 32px
+        margin-right 9px
+        vertical-align middle
+        background-size 30px 32px
+        bg-image('logo')
+  /*===头部标题==================================================================================================================================================*/
+    .header-txt
+        display inline-block
+        color: $color-theme
+        font-size: $font-size-large
+        vertical-align middle
+</style>
---
+++ src/components/tab/Tab.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="tab">
+    <router-link tag="div" to="/recommend" class="tab-item tab-recommend">
+      <div class="tab-link">推荐</div>
+    </router-link>
+    <router-link tag="div" to="/singer" class="tab-item tab-singer">
+      <div class="tab-link">歌手</div>
+    </router-link>
+    <router-link tag="div" to="/rank" class="tab-item tab-rank">
+      <div class="tab-link">排名</div>
+    </router-link>
+    <router-link tag="div" to="/search" class="tab-item tab-search">
+      <div class="tab-link">搜索</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tab'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~common/stylus/variable.styl';
+  .tab
+    display flex
+    width 100%
+    height: 35px
+    line-height: 35px
+    font-size: $font-size-medium
+    margin-bottom 15px
+  /*===tab栏子项公共样式===============================================================================================================================*/
+    .tab-item
+      flex 1
+      text-align center
+      .tab-link
+        margin 0 25px
+        color: $color-text-l
+    &.router-link-active
+      .tab-link
+        color: $color-theme
+        border-bottom: 2px solid $color-theme
+</style>
