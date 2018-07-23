cccd70ef2cd6ac963c42fcf957a1c8a56f77ef45
James-Chann <jameschan51@163.com>
2018-07-23 16:52:57 +0800

    singer-style finish :bowtie:one step:smirk:

--- src/components/singer/Singer.vue
+++ src/components/singer/Singer.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="singer">
+    <list-view :data="singers"></list-view>
   </div>
 </template>
 
 <script>
 import {getSingerList} from 'api/singer'
 import {ERR_OK} from 'api/config'
+import Singer from 'common/js/singer'
+import ListView from 'base/listview/listview'
 
-// const HOT_NAME = '热门'
-// const HOT_SINGER_LEN = 10
+const HOT_SINGER_LEN = 10
+const HOT_NAME = '热门'
 
 export default {
   name: 'Singer',
+  components: {
+    ListView
+  },
   data () {
     return {
       singers: []
@@ -24,31 +30,58 @@ export default {
     _getSingerList() {
       getSingerList().then((res) => {
         if (res.code === ERR_OK) {
-          this.singers = res.data.list
-          console.log(this.singers)
+          this.singers = this._normalizeSinger(res.data.list)
         }
       })
+    },
+    _normalizeSinger(list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        }
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer({
+          id: item.Fsinger_mid,
+          name: item.Fsinger_name
+        }))
+      })
+      // 为了得到有序列表，我们需要处理 map
+      let ret = []
+      let hot = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
     }
-    // _normalizeSinger (list) {
-    //   let map = {
-    //     hot: {
-    //       title: HOT_NAME,
-    //       items: []
-    //     }
-    //   }
-    //   list.forEach((item, index) => {
-    //     if (index < HOT_SINGER_LEN) {
-    //       map.hot.items.push({
-
-    //       })
-    //     }
-    //   })
-    // }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
+@import '~common/stylus/variable.styl';
   .singer
     position fixed
     top 88px
